5cdeb0964b1864f2664fe34f3a15a4513f05a906
KevinDu10 <kevinwd2401@gmail.com>
2026-04-13 16:49:16 -0400

    Throwable base ingredient

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] float timer;
+    private int reputation;
+    public int Reputation {get {return reputation;}
+        set {
+            if (gameOver) return;
+            reputation = value;
+            if (reputation <= 0) {
+                reputation = 0;
+                //end game
+            }
+        }}
+    bool gameOver;
+
+    void Awake() {
+        Instance = this;
+        reputation = 100;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (gameOver) return;
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            gameOver = true;
+            timer = 0;
+            // end game
+        }
+    }
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,47 @@
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(Ingredient))]
+public class Throwable : MonoBehaviour
+{
+    bool hurtsPlayer;
+    Ingredient ing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hurtsPlayer = true;
+        ing = GetComponent<Ingredient>();
+    }
+
+    void OnCollisionEnter(Collision c) {
+        ing.ThrowableOnCollide(c);
+        if (c.gameObject.tag == "Floor") {
+            RemoveIngredient();
+        }
+        else {
+            hurtsPlayer = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider c) {
+        ing.ThrowableOnTrigger(c);
+        if (c.tag == "Player" && hurtsPlayer) {
+            GameManager.Instance.Reputation -= 10;
+        } else if (c.tag == "NPC" && !hurtsPlayer) {
+            //stun NPC
+        }
+    }
+
+    private void RemoveIngredient() {
+        // explode vfx?
+        Destroy(gameObject);
+    }
+
+    public void OnGrab(SelectEnterEventArgs args) {
+        Debug.Log("Ingredient Grabbed");
+        hurtsPlayer = false;
+    }
+}
