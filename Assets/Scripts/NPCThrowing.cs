ad3919c0a0215fe3f1c44e48b160fc2571924621
Yufei-Xia <77976238+Yufei-Xia@users.noreply.github.com>
2026-04-19 05:31:12 -0400

    Update NPCThrowing.cs

--- Assets/Scripts/NPCThrowing.cs
+++ Assets/Scripts/NPCThrowing.cs
@@ -1,110 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class NPCThrowing : MonoBehaviour
+[RequireComponent(typeof(Ingredient))]
+public class Throwable : MonoBehaviour
 {
-    [SerializeField] Transform spawnPt;
-    public Material[] tempMats;
-    MeshRenderer mr;
-    bool canThrow;
-    // Start is called before the first frame update
+    private XRGrabInteractable grabInteractable;
+    public bool hurtsPlayer = false;
+    Ingredient ing;
     void Start()
     {
-        StartCoroutine(ThrowCor());
-        mr = GetComponent<MeshRenderer>();
-        tempMats[0] = mr.material;
+        ing = GetComponent<Ingredient>();
     }
-    void Update() {
 
-        if (canThrow)
-        {
-            Vector3 targetDirection;
-            Vector3 toPlayer = GameManager.Instance.playerTrans.position - transform.position;
-            targetDirection = new Vector3(toPlayer.x, 0f, toPlayer.z);
+    public void InitializeThrown() {
+        hurtsPlayer = true;
+    }
 
-            if (targetDirection.sqrMagnitude > 0.001f)
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-                transform.rotation = Quaternion.Slerp(
-                    transform.rotation,
-                    targetRotation,
-                    Time.deltaTime * 5f
-                );
-            }
+    void OnCollisionEnter(Collision c) {
+        ing.ThrowableOnCollide(c);
+        if (c.gameObject.tag == "Floor") {
+            RemoveIngredient();
         }
-        else
-        {
-            transform.rotation = transform.parent.rotation;
+        else {
+            hurtsPlayer = false;
         }
     }
 
-    private IEnumerator ThrowCor() {
-        yield return new WaitForSeconds(5);
-        while (true) {
-            if (canThrow) {
-                mr.material = tempMats[1];
-                yield return new WaitForSeconds(1);
-                Vector3 r = Random.insideUnitSphere;
-                r.y = 0;
-                ThrowAtPlayer(GameManager.Instance.playerTrans.position + r + 0.1f * Vector3.up);
-                yield return new WaitForSeconds(0.2f);
-                mr.material = tempMats[0];
-            }
-            yield return new WaitForSeconds(14 + 6 *  Random.value);
-        }
 
-    }
-
-    private void ThrowAtPlayer(Vector3 target, float speed = 10)
+    void Awake()
     {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+    }
 
-        Vector3 start = spawnPt.position;
+    void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(OnGrab);
+    }
 
-        Vector3 toTarget = target - start;
-        Vector3 toTargetXZ = new Vector3(toTarget.x, 0f, toTarget.z);
+    void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+    }
 
-        float x = toTargetXZ.magnitude;   // horizontal distance
-        float y = toTarget.y;             // vertical difference
-        float g = Mathf.Abs(Physics.gravity.y);
-        float v = speed;
+    public void RemoveIngredient() {
+        Destroy(gameObject);
+    }
 
-        float v2 = v * v;
-        float discriminant = v2 * v2 - g * (g * x * x + 2f * y * v2);
+    [SerializeField] private float maxCatchDistance = 3.0f;
 
-        // Target cannot be hit with this speed
-        if (discriminant < 0f) {
-            Debug.Log("Cannot throw"); 
+    public void OnGrab(SelectEnterEventArgs args)
+    {
+        float dist = Vector3.Distance(args.interactorObject.transform.position, transform.position);
+
+        if (dist > maxCatchDistance)
+        {
+            if (args.interactorObject is IXRSelectInteractor interactor)
+            {
+                XRInteractionManager manager = grabInteractable.interactionManager != null
+                    ? grabInteractable.interactionManager
+                    : FindObjectOfType<XRInteractionManager>();
+
+                if (manager != null)
+                    manager.SelectExit(interactor, grabInteractable);
+            }
             return;
         }
 
-        if (GameManager.Instance.throwingList.Length == 0) return;
-
-        GameObject prefab = GameManager.Instance.throwingList[Random.Range(0, GameManager.Instance.throwingList.Length)];
-        GameObject proj = Instantiate(prefab, spawnPt.position, Quaternion.identity);
-
-        Rigidbody rb = proj.GetComponent<Rigidbody>();
-        if (rb == null) return;
-        proj.GetComponent<Throwable>().InitializeThrown();
-
-        float sqrt = Mathf.Sqrt(discriminant);
-
-        float tanTheta = (Random.value > 1f) // use low arc?
-            ? (v2 + sqrt) / (g * x)
-            : (v2 - sqrt) / (g * x);
-
-        float angle = Mathf.Atan(tanTheta);
-
-        Vector3 dir = toTargetXZ.normalized;
-
-        Vector3 velocity =
-            dir * (v * Mathf.Cos(angle)) +
-            Vector3.up * (v * Mathf.Sin(angle));
-
-        rb.velocity = velocity;
+        Debug.Log("Ingredient Grabbed");
+        hurtsPlayer = false;
     }
 
-    public void SetThrow(bool throwing) {
-        canThrow = throwing;
+    public void MagnetCatch(Transform hand)
+    {
+        hurtsPlayer = false;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+        }
+
+        transform.position = hand.position;
+        transform.rotation = hand.rotation;
+        transform.SetParent(hand);
     }
 }
+
